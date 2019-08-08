159e6202e3b5f341e8e39458aa612775ffe9df75
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-08 14:31:15 -0300

    Plugin's init.

---
+++ init.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'redmine'
+
+Redmine::Plugin.register :eac_redmine_base0 do
+  name 'EacRedmineBase0'
+  author 'Eduardo Henrique Bogoni'
+  description 'E.A.C. base for Redmine.'
+  version '0.0.0'
+end
