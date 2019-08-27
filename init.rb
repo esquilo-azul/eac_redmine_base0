ad0636e23d37715604b1cd6248863400170f7141
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-27 14:35:30 -0300

    Bump to '0.1.0'.

--- init.rb
+++ init.rb
@@ -6,5 +6,5 @@ Redmine::Plugin.register :eac_redmine_base0 do
   name 'EacRedmineBase0'
   author 'Eduardo Henrique Bogoni'
   description 'E.A.C. base for Redmine.'
-  version '0.0.0'
+  version '0.1.0'
 end
