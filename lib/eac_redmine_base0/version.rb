f8869f08334d992a9453c96066381589614de820
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2024-03-15 21:32:26 -0300

    Bump to '0.6.1'.

--- lib/eac_redmine_base0/version.rb
+++ lib/eac_redmine_base0/version.rb
@@ -4,5 +4,5 @@ module EacRedmineBase0
   AUTHOR = 'Eduardo Henrique Bogoni'
   HOMEPAGE = 'https://github.com/esquilo-azul/eac_redmine_base0'
   SUMMARY = 'E.A.C. base for Redmine.'
-  VERSION = '0.6.0'
+  VERSION = '0.6.1'
 end
